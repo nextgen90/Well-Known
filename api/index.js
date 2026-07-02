eae3fe8140d8b939b65f03fd9b8a0664194656bf
rohit kumar <nextgenearners352007@gmail.com>
2026-07-02 11:25:41 +0530

    fix

--- api/index.js
+++ api/index.js
@@ -1,10 +1,14 @@
-const serverless = require('serverless-http');
 const { app, initDatabase } = require('../server');
+
+let initialized = false;
 
 module.exports = async (req, res) => {
   try {
-    await initDatabase();
-    return serverless(app)(req, res);
+    if (!initialized) {
+      await initDatabase();
+      initialized = true;
+    }
+    return app(req, res);
   } catch (err) {
     console.error('Vercel function failed to initialize:', err);
     res.status(500).send('Server initialization failed');
